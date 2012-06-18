4c97a9291e58eaf528c5b7169c7306b7d29cded5
Felix Bytow <felix.bytow@googlemail.com>
2012-06-18 14:11:18 +0200

    added UTF-8/UTF-16 conversions

--- src/conversion/encoding_cast.cxx
+++ src/conversion/encoding_cast.cxx
@@ -37,7 +37,7 @@ namespace utf8
                 {
                     std::wstring result;
                     result.reserve(src.length());
-                    for (auto const & c : src)
+                    for (auto c : src)
                     {
                         result.push_back(c);
                     }
@@ -47,7 +47,7 @@ namespace utf8
                 static utf8::string from_wstring(std::wstring const & src)
                 {
                     utf8::string result;
-                    for (auto const & c : src)
+                    for (auto c : src)
                     {
                         result.push_back(c);
                     }
@@ -60,14 +60,42 @@ namespace utf8
             {
                 static std::wstring to_wstring(utf8::string const & src)
                 {
-                    // TODO: implement UTF-8 to UTF-16 conversion
-                    return std::wstring();
+                    std::wstring result;
+                    result.reserve(src.length() * 2);
+                    for (auto c : src)
+                    {
+                        if (c <= 0xFFFF)
+                            result.push_back(static_cast<wchar_t>(c));
+                        else
+                        {
+                            // build surrogate pair
+                            c -= 0x10000;
+                            wchar_t high    = (0x36 << 10) | ((c >> 10) & 0x3FF),
+                                    low     = (0x37 << 10) | (c & 0x3FF);
+                            result.push_back(high);
+                            result.push_back(low);
+                        }
+                    }
+                    return result;
                 }
 
                 static utf8::string from_wstring(std::wstring const & src)
                 {
-                    // TODO: implement UTF-16 to UTF-8 conversion
-                    return utf8::string();
+                    utf8::string result;
+                    value_t val = 0;
+                    for (auto c : src)
+                    {
+                        if ((c >> 10) == 0x36) // start of surrogate
+                            val = c & 0x3FF;
+                        else if ((c >> 10) == 0x37) // end of surrogate
+                        {
+                            result.push_back(0x10000 + ((val << 10) | (c & 0x3FF)));
+                            val = 0;
+                        }
+                        else
+                            result.push_back(c);
+                    }
+                    return result;
                 }
             };
         }
